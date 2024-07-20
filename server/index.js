e8c9536f41439f48f9cd4c4a7955bce1ca79c76e
daniel70125 <dwrighttt504@gmail.com>
2024-07-20 15:27:09 -0500

    updated things

---
+++ server/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+
+const merchant_model = require('./merchant_model');
+
+const path = require('path'); // Usually moved to the start of file
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+app.use( express.static( `${__dirname}/../build` ) );
+app.use(express.json())
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  next();
+});
+
+
+app.get('/users', (req, res) => {
+    merchant_model.getMerchants()
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    })
+  })
+
+  app.post('/animals', (req, res) => {
+    merchant_model.createMerchant(req.body)
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    })
+  })
+
+app.listen(4000, () => console.log("Server running on Port 4000!"));
